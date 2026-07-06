23acb5ef9ce53219af0beeb79fbd31c76235d0f4
Atin Dimri <148791111+AtinDimri@users.noreply.github.com>
2026-07-06 13:21:29 +0530

    Update print statements and loop range in PUSHING.py

--- PUSHING.py
+++ PUSHING.py
@@ -1,9 +1,9 @@
-print("Pushing testing docking")
+print("Pushing testing good testing best docking")
 
 
 print("My logic THIS THIS TEstIng for this code")
 
-for i in range(12):
+for i in range(18):
     print("This is iteration number:", i)
 
 
